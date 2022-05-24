50ff1a586e4d8578d70560344f4cc8c430e871a8
RokibulHasan7 <mdrokibulhasan18@gmail.com>
2022-05-24 19:24:53 +0600

    Cart functionality updated.

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from audioop import reverse
 from pydoc import describe
+from tkinter import CASCADE
 from django.db import models
 from django.urls import reverse
 from category.models import Category
@@ -23,3 +24,30 @@ class Product(models.Model):
     
     def __str__(self):
         return self.product_name
+
+
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)
+
+
+variation_category_choice = (
+    ('color', 'color'),
+    ('size', 'size'),
+)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    objects = VariationManager()
+
+    def __str__(self):
+        return self.variation_value
+    
