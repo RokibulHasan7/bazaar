08c6d718e3d2f33aea595bd1dc8049ad03dbb810
RokibulHasan7 <mdrokibulhasan18@gmail.com>
2022-05-25 12:24:14 +0600

    User Activation & Activation Link created.

--- bazaar/settings.py
+++ bazaar/settings.py
@@ -135,3 +135,18 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+
+from django.contrib.messages import  constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+
+#SMTP configuration
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'abrakidabra177@gmail.com'
+EMAIL_HOST_PASSWORD = 'tornanirheda1234!'
+EMAIL_USE_TLS = True
